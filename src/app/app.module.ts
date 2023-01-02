fa1290f5c94220dbe20eec23b1c632b0540cc6d9
RadwaElhosiny <radwaa.elhosiny@gmail.com>
2023-01-02 16:38:44 +0200

    import leaflet module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/maps/maps.module.ts
+++ src/app/modules/maps/maps.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MapsComponent } from './maps.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 
 @NgModule({
   declarations: [MapsComponent],
   imports: [
     CommonModule,
-    MapsRoutingModule
+    MapsRoutingModule,
+    LeafletModule
   ]
 })
 export class MapsModule { }
