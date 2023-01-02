009b5139bd7a76166d04db713e4da179035eece6
RadwaElhosiny <radwaa.elhosiny@gmail.com>
2023-01-02 16:40:03 +0200

    replace leaflet js with ngx-leaflet

--- src/app/modules/maps/maps.component.ts
+++ src/app/modules/maps/maps.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
 import {MapService} from "../../shared/services/map/map.service";
 import {Router} from "@angular/router";
+import 'leaflet/dist/images/marker-shadow.png';
 
 @Component({
   selector: 'app-maps',
@@ -10,11 +11,12 @@ import {Router} from "@angular/router";
 })
 export class MapsComponent implements AfterViewInit {
 
-  map;
   mapCoordinates;
-  mapMarker;
   lat;
   lng;
+  mapOptions;
+  layers;
+  
 
   constructor(private mapService: MapService, private router: Router) { }
 
@@ -24,38 +26,28 @@ export class MapsComponent implements AfterViewInit {
     while (this.mapCoordinates.length > 0) {
       arrays.push(this.mapCoordinates.splice(0, size));
     }
-
-
     console.log(arrays);
   }
   async getMapCoordinates() {
     for (let i = 1; i < this.mapCoordinates.length; i++) {
       this.lat = this.mapCoordinates[i].lat;
       this.lng = this.mapCoordinates[i].lng;
-      this.mapMarker.setLatLng([this.lat, this.lng]);
+      this.layers = [ L.marker([this.lat, this.lng])];
       await new Promise(resolve => setTimeout(resolve, 2000));
     }
   }
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ this.lat, this.lng],
-      zoom: 15
-    });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 1,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    this.mapOptions = {
+      layers: [
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 10,
+    center: L.latLng(this.lat,this.lng)
+  }
 
-    tiles.addTo(this.map);
-    const icon = L.icon({
-      iconUrl: 'assets/images/marker-icon.png',
-      popupAnchor: [13, 0],
-    });
-
-    this.mapMarker = L.marker([ this.lat, this.lng ], { icon }).bindPopup('Angular Leaflet');
-    this.mapMarker.addTo(this.map);
+  this.layers = [
+    L.marker([this.lat, this.lng], )];
   }
 
   uniqueArrayOfCoordinates(originalArray) {
@@ -84,7 +76,4 @@ export class MapsComponent implements AfterViewInit {
     sessionStorage.clear();
     this.router.navigate(['/login']);
   }
-
-
-
 }
